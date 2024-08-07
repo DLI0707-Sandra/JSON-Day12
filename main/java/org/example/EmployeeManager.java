76f2529c4a6f6695cf8ef59470fcc97fe6b7d7a5
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-07 17:51:31 +0530

    Getting employee object and modifying field

--- main/java/org/example/EmployeeManager.java
+++ main/java/org/example/EmployeeManager.java
@@ -3,23 +3,45 @@ package org.example;
 import com.google.gson.Gson;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class EmployeeManager
 {
     public static void main(String[] args)
     {
         Gson gson=new Gson();
-
         List<Employee> employees_list =new ArrayList<>();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the name of the employee to be updated:");
+        String name=scanner.next();
 
         try(FileReader fileReader=new FileReader("employee.json");)
         {
             Employee[] employees=gson.fromJson(fileReader,Employee[].class);
             employees_list.addAll(Arrays.asList(employees));
+            for(Employee employee:employees_list)
+            {
+                if(employee.getName().equals(name))
+                {
+                    System.out.println("Enter the field to be modified:");
+                    String field=scanner.next();
+                    switch (field)
+                    {
+                        case "age":
+                            System.out.println("Enter the new value:");
+                            employee.setAge(scanner.nextInt());
+                            break;
+                        case "address": System.out.println("Enter the new value:");
+                            employee.setAddress(scanner.next());
+                            break;
+                    }
+                }
+            }
 
         }catch(IOException e)
         {
@@ -28,6 +50,15 @@ public class EmployeeManager
 
         System.out.println("List of employees:");
         System.out.println(employees_list);
+
+        try(FileWriter fileWriter=new FileWriter("employee.json");)
+        {
+            Employee[] updated=employees_list.toArray(new Employee[0]);
+            gson.toJson(updated,fileWriter);
+
+        }catch (IOException e){
+            System.out.println("IO exception!");
+        }
 
     }
 }
